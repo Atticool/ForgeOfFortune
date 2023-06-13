f12a4b022387ba11bf0a48779d0fbf596bb7179a
Marcus Harrison-Banuelos <marcus.harrisonbanueloa@studytafensw.edu.au>
2023-06-13 15:27:16 +1000

    fixed tabs

--- src/main/java/com/teamelemental/forgeoffortune/item/ModCreativeModeTabs.java
+++ src/main/java/com/teamelemental/forgeoffortune/item/ModCreativeModeTabs.java
@@ -6,12 +6,14 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = ForgeOfFortune.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeModeTabs {
     public static CreativeModeTab FOF_TAB;
 
+    @SubscribeEvent
     public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
         FOF_TAB = event.registerCreativeModeTab(new ResourceLocation(ForgeOfFortune.MOD_ID, "fof_tab"),
             builder -> builder.icon(() -> new ItemStack(ModItems.BLINGOT.get()))
