9dec557df47d02a9ca103ec505a013a0cd71c14d
Marcus Harrison-Banuelos <marcus.harrisonbanueloa@studytafensw.edu.au>
2023-06-13 19:46:55 +1000

    made blingus logs rotatable. im in so much pain

--- src/main/java/com/teamelemental/forgeoffortune/block/ModBlocks.java
+++ src/main/java/com/teamelemental/forgeoffortune/block/ModBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -23,23 +24,11 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, ForgeOfFortune.MOD_ID);
 
-    public static final RegistryObject<Block> BLINGUS_ORE = registerBlock("blingus_ore",
-            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE), UniformInt.of(2,6)));
-    public static final RegistryObject<Block> DEEPSLATE_BLINGUS_ORE = registerBlock("deepslate_blingus_ore",
-            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(3f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE), UniformInt.of(2, 6)));
-
-    public static final RegistryObject<Block> BLINGUS_BLOCK = registerBlock("blingus_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(6f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
-
-    public static final RegistryObject<Block> BLINGUS_LOG = registerBlock("blingus_log",
-            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
-                    .strength(4f).sound(SoundType.WOOD)));
-    public static final RegistryObject<Block> BLINGUS_PLANKS = registerBlock("blingus_wood_planks",
-            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
-                    .strength(4f).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> BLINGUS_ORE = registerBlock("blingus_ore",() -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.METAL).strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE), UniformInt.of(2,6)));
+    public static final RegistryObject<Block> DEEPSLATE_BLINGUS_ORE = registerBlock("deepslate_blingus_ore",() -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.METAL).strength(3f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE), UniformInt.of(2, 6)));
+    public static final RegistryObject<Block> BLINGUS_BLOCK = registerBlock("blingus_block",() -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+    public static final RegistryObject<Block> BLINGUS_LOG = registerBlock("blingus_log",() -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(4f).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> BLINGUS_PLANKS = registerBlock("blingus_wood_planks",() -> new Block(BlockBehaviour.Properties.of(Material.WOOD).strength(4f).sound(SoundType.WOOD)));
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
